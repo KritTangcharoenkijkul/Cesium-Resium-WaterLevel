a5d934bf0eec61bd9e22d270a1845f30e4735b1e
KritTangcharoenkijkul <48550332+KritTangcharoenkijkul@users.noreply.github.com>
2020-06-06 16:25:51 +0700

    Web Map Server WMS on Testingtest2

--- src/App.js
+++ src/App.js
@@ -127,9 +127,9 @@ import './App.css'
 BingMapsApi.defaultKey = 'Apwuw8J82n9rnuXUcLTiiqerkws2jLzK_UWa_as9CXtLnvL9py71qGumTgmAPbde';
 Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMDk2ZmI1ZC00M2M1LTQwMTktYmRiZS0xNDRhODE0YTA2ZjYiLCJpZCI6MjM5NjcsInNjb3BlcyI6WyJhc2wiLCJhc3IiLCJhc3ciXSwiaWF0IjoxNTg2OTc1NDIyfQ.-nb9o-e7NJ-k5xfdXteCeZxLQ53yuMuFV9Z80PrYL88';
 // Model
-const origin = Cartesian3.fromDegrees(100.7783, 13.7628, 50);
-const cameraDest = Cartesian3.fromDegrees(100.6456, 13.7628, 50);
-const cameraDest3 = Cartesian3.fromDegrees(100.6087, 13.8242, 50);
+const origin = Cartesian3.fromDegrees(100.7783, 13.7328, 50);
+const cameraDest = Cartesian3.fromDegrees(100.6356, 13.7628, 50);
+const cameraDest3 = Cartesian3.fromDegrees(100.5987, 13.8242, 50);
 const modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
 const modelMatrix2 = Transforms.eastNorthUpToFixedFrame(cameraDest);
 const modelMatrix3 = Transforms.eastNorthUpToFixedFrame(cameraDest3);
@@ -149,19 +149,19 @@ const App = () => (
   </addToolbarMenu>
     <Model url={glbs}
     modelMatrix={modelMatrix}
-    minimumPixelSize={128}
+    minimumPixelSize={250}
     maximumScale={20000}
      />
 
     <Model url={glbs}
     modelMatrix={modelMatrix2}
-    minimumPixelSize={128}
+    minimumPixelSize={250}
     maximumScale={20000}
      />
 
     <Model url={glbs}
     modelMatrix={modelMatrix3}
-    minimumPixelSize={128}
+    minimumPixelSize={250}
     maximumScale={20000}
      />
 
